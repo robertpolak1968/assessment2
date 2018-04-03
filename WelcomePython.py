ebfbdc8b03fa69aa7ad3ff3569cd13a31dffdcb9
Robert Polak <robertpolak1968@gmail.com>
2018-04-03 10:36:28 +0100

    WelcomePython.py 3rd

--- WelcomePython.py
+++ WelcomePython.py
@@ -6,4 +6,8 @@ print ("Welcome",getpass.getuser()+",")
 print ("Another Beautiful Day !!!")
 print ()
 now = datetime.datetime.now()
+print ("Current date and time : ", now.strftime("%Y-%m-%d %H:%M:%S"))
+print ()
+cwd = os.getcwd()
+print ("Current diretory : ", cwd)
 
